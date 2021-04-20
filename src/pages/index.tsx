784a8c6d7ea7aa849cdd00ce07fbc0da6d2a9a7d
joshjess <joshuajessy47@gmail.com>
2021-04-20 11:11:09 +0200

    Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,29 +17,36 @@ const Home: React.FC = () => (
         <div id="title-header">
           <Title level={1}>CSR vs SSR vs SSG</Title>
         </div>
-        <Paragraph>
+        <Paragraph className="paragraph">
           Welcom to my website! I made this website to educate people on the
-          topic of different React rendering methods. The rendering methods i
-          will talk about on this website are Client Side Rendering(SSG), Server
-          Side Rendering(SSR) and Static Site Generation(SSG). This website is
-          made up of different pages one for each rendering method the page is
-          also renderd using the coresponing method.
+          topic of different React rendering methods. Although that this website
+          used the React framework the principles also carryover to other
+          frameworks like Angular and Vue. On this website you can find three
+          pages the content is the same, but they are rendered differently.The
+          rendering methods i will talk about on this website are Client Side
+          Rendering(SSG), Server Side Rendering(SSR) and Static Site
+          Generation(SSG). This website is made up of different pages one for
+          each rendering method the page is also renderd using the coresponing
+          method.If you read further on this page you can read more about the
+          different rendering methods and there pros and cons you can also see
+          an image with the google lighthouse score so you can test it yourself.
         </Paragraph>
-        <Paragraph>
-          Suspendisse tellus sem, sollicitudin pulvinar odio eget, blandit
-          pharetra magna. Suspendisse lobortis mattis nisl, at sagittis dui
-          commodo et. Proin et erat eu dui semper auctor ut at dui. Maecenas
-          nisl neque, fermentum tincidunt fringilla quis, consequat sed nisl.
-          Morbi non justo purus. Etiam in tempor sapien. Nunc ornare nisi
-          libero, non maximus lorem vestibulum et. Mauris ullamcorper, nulla et
-          vehicula ullamcorper, nibh enim molestie massa, non rhoncus leo massa
-          sit amet lorem. Cras eleifend scelerisque porta. Fusce rutrum, risus
-          eget lobortis tempor, enim eros scelerisque quam, eget elementum purus
-          erat et nisl. Proin sit amet dolor at est sodales elementum ut quis
-          ex. Fusce sed condimentum dolor, nec aliquet ante. Curabitur
-          ullamcorper arcu sed nisl sollicitudin vestibulum. Aenean sodales
-          consectetur ante, vitae posuere enim imperdiet eget.
+        <Title level={3}>Client Side Rendering</Title>
+        <Paragraph className="paragraph">
+          When the website is using client side rendering the server sends a
+          blank page with links to JavaScript code. The JavaScript code gets
+          executed on the browser and renders the HTML elements.
         </Paragraph>
+        <Title level={5}>Pros</Title>
+        <ul>
+          <li>Easy in development</li>
+          <li>Low server load</li>
+        </ul>
+        <Title level={5}>Cons</Title>
+        <ul>
+          <li>Bad SEO performance out of the box</li>
+          <li>Longer initial rendering time</li>
+        </ul>
         <Row justify="space-around">
           <Col span={8}>
             <Image src={imageUrl} alt="nature" width={500} height={500} />
